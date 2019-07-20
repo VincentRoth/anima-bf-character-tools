91fbfb114d04fc06a69bc297bd40fd71ca668f20
Vincent Roth <vincent.roth@live.fr>
2019-07-21 00:37:36 +0200

    Table - new tables and cell style

--- src/app/shared/table/table.component.ts
+++ src/app/shared/table/table.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ReferenceTable } from '../reference-table/reference-table.model';
 
+const boldItalicRegExp = /^\*\*\*/;
+const boldRegExp = /^\*\*/;
+const italicRegExp = /^\*/;
+
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
@@ -12,4 +16,35 @@ export class TableComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  get caption(): string {
+    if (typeof this.table.id === 'number') {
+      return `Tableau ${this.table.id} : ${this.table.title}`;
+    }
+    if (this.table.id.startsWith('p.')) {
+      return `${this.table.id} : ${this.table.title}`;
+    }
+    return `Encadré ${this.table.id} : ${this.table.title}`;
+  }
+
+  getCellStyle(value: any): string {
+    if (typeof value === 'string') {
+      if (boldItalicRegExp.test(value)) {
+        return 'bold italic';
+      }
+      if (boldRegExp.test(value)) {
+        return 'bold';
+      }
+      if (italicRegExp.test(value)) {
+        return 'italic';
+      }
+    }
+  }
+
+  formatValue(value: any): string {
+    if (typeof value === 'string' && value.startsWith('*')) {
+      return value.replace(/^\**/, '');
+    }
+    return value;
+  }
 }
