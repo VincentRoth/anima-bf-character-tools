1eb161461cfd9994d1e190e8e7cef44f2ba058db
Vincent Roth <vincent.roth@live.fr>
2019-08-03 11:30:09 +0200

    Change table button to add to character

--- src/app/shared/table/table.component.spec.ts
+++ src/app/shared/table/table.component.spec.ts
@@ -1,6 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AppMaterialModule } from 'src/app/app-material.module';
 import { TableComponent } from './table.component';
 
 describe('TableComponent', () => {
@@ -9,8 +8,7 @@ describe('TableComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [TableComponent],
-      imports: [AppMaterialModule]
+      declarations: [TableComponent]
     }).compileComponents();
   }));
 
