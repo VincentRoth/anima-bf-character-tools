342df77f8e1d74374cf2ef27841f3b8570e56793
VincentRoth <vincent.roth@live.fr>
2021-10-20 19:42:28 +0200

    Rework abstract query once

--- src/app/shared/services/abstract-query-once.service.ts
+++ src/app/shared/services/abstract-query-once.service.ts
@@ -1,36 +1,42 @@
 import { HttpClient } from '@angular/common/http';
 import cloneDeep from 'lodash-es/cloneDeep';
-import { Observable, of, Subject } from 'rxjs';
+import { Observable, ReplaySubject, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+class ReplaySubjectWithValue<T> extends ReplaySubject<T> {
+  value: T;
+
+  next(value?: T): void {
+    this.value = value;
+    super.next(value)
+  }
+}
+
 export abstract class AbstractQueryOnceService<T> {
-  protected data: T;
-  protected request: Subject<T>;
+  protected data$: ReplaySubjectWithValue<T> = new ReplaySubjectWithValue<T>();
+  private dataSubscription: Subscription;
 
-  constructor(private http: HttpClient, protected url: string) {
-    this.request = new Subject<T>();
-
-    this.http.get<T>(url).subscribe({
-      next: (data) => {
-        this.data = this.transformData(data);
-        this.request.next(data);
-        this.request.complete();
-      }
-    });
+  protected get data(): T {
+    return this.data$.value;
   }
 
+  constructor(private http: HttpClient, private url: string) { }
+
   get(): Observable<T> {
-    if (!this.data) {
-      return this.getData().pipe(map(cloneDeep));
+    if (this.data === undefined && !this.dataSubscription) {
+      this.fetchData();
     }
-    return of(cloneDeep(this.data));
+    return this.data$.asObservable().pipe(map(cloneDeep));
+  }
+
+  protected fetchData(): void {
+    this.dataSubscription = this.http.get<T>(this.url).subscribe((data) => {
+      this.data$.next(this.transformData(data));
+      this.data$.complete();
+    });
   }
 
   protected transformData(data: T): T {
     return data;
-  }
-
-  private getData(): Observable<T> {
-    return this.request.asObservable();
   }
 }
